35a9adc4db65cf0b7a874b8789e9a65042c492d3
José Victor <josevictor.costa@beblue.com.br>
2019-04-16 08:40:24 -0300

    Protocol Composition

--- TheSwiftProgrammingLanguage.playground/Pages/Protocols.xcplaygroundpage/Contents.swift
+++ TheSwiftProgrammingLanguage.playground/Pages/Protocols.xcplaygroundpage/Contents.swift
@@ -486,3 +486,37 @@ extension SnakesAndLadders: PrettyTextRepresentable {
         return output
     }
 }
+
+/*Class-Only Protocols*/
+//You can limit protocol adoption to class types (and not structures or enumerations) by adding the AnyObject protocol to a protocol’s inheritance list.
+
+protocol SomeInheritedProtocol {
+    
+}
+
+protocol SomeClassOnlyProtocol: AnyObject, SomeInheritedProtocol {
+    // class-only protocol definition goes here
+}
+//
+//In the example above, SomeClassOnlyProtocol can only be adopted by class types. It’s a compile-time error to write a structure or enumeration definition that tries to adopt SomeClassOnlyProtocol
+//Proof:
+
+class TestClassOnlyProtocol: SomeClassOnlyProtocol {
+    
+}
+// It works...
+
+struct TestStructClassOnlyProtocol: SomeClassOnlyProtocol {
+    
+}
+
+// It not works...
+//error: non-class type 'TestStructClassOnlyProtocol' cannot conform to class protocol 'SomeClassOnlyProtocol'
+//struct TestStructClassOnlyProtocol: SomeClassOnlyProtocol {
+//^
+
+//NOTE
+//
+//Use a class-only protocol when the behavior defined by that protocol’s requirements assumes or requires that a conforming type has reference semantics rather than value semantics.
+
+/*Protocol Composition*/
