1b7c6c322f3678e3177d4b117ab4b1d1bebe7e5a
José Victor <josevictor.costa@beblue.com.br>
2019-04-16 08:29:14 -0300

    Protocol Inheritance

--- TheSwiftProgrammingLanguage.playground/Pages/Protocols.xcplaygroundpage/Contents.swift
+++ TheSwiftProgrammingLanguage.playground/Pages/Protocols.xcplaygroundpage/Contents.swift
@@ -456,3 +456,33 @@ for thing in things {
 }
 
 /*Protocol Inheritance*/
+//A protocol can inherit one or more other protocols and can add further requirements on top of the requirements it inherits. The syntax for protocol inheritance is similar to the syntax for class inheritance, but with the option to list multiple inherited protocols, separated by commas:
+
+protocol InheritingProtocol: SomeProtocol, AnotherProtocol {
+    // protocol definition goes here
+}
+
+protocol PrettyTextRepresentable: TextRepresentable {
+    var prettyTextualDescription: String { get }
+}
+
+//This example defines a new protocol, PrettyTextRepresentable, which inherits from TextRepresentable. Anything that adopts PrettyTextRepresentable must satisfy all of the requirements enforced by TextRepresentable, plus the additional requirements enforced by PrettyTextRepresentable. In this example, PrettyTextRepresentable adds a single requirement to provide a gettable property called prettyTextualDescription that returns a String.
+
+//The SnakesAndLadders class can be extended to adopt and conform to PrettyTextRepresentable:
+
+extension SnakesAndLadders: PrettyTextRepresentable {
+    var prettyTextualDescription: String {
+        var output = textualDescription + ":\n"
+        for index in 1...finalSquare {
+            switch board[index] {
+            case let ladder where ladder > 0:
+                output += "▲ "
+            case let snake where snake < 0:
+                output += "▼ "
+            default:
+                output += "○ "
+            }
+        }
+        return output
+    }
+}
