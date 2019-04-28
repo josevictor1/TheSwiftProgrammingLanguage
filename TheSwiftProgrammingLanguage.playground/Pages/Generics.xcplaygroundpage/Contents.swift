58792cf175e0588764d1b1cb4df8217f225cad39
José Victor <josevictor.costa@beblue.com.br>
2019-04-28 11:16:01 -0300

    Extending a Generic Type

--- TheSwiftProgrammingLanguage.playground/Pages/Generics.xcplaygroundpage/Contents.swift
+++ TheSwiftProgrammingLanguage.playground/Pages/Generics.xcplaygroundpage/Contents.swift
@@ -1,4 +1,5 @@
 import Foundation
+import UIKit
 
 /*Generics*/
 
@@ -90,4 +91,88 @@ swapTwoValues(&someString, &anotherString)
 //
 //You can provide more than one type parameter by writing multiple type parameter names within the angle brackets, separated by commas.
 
+//NOTE
+//
+//The concept of a stack is used by the UINavigationController class to model the view controllers in its navigation hierarchy. You call the UINavigationController class pushViewController(_:animated:) method to add (or push) a view controller on to the navigation stack, and its popViewControllerAnimated(_:) method to remove (or pop) a view controller from the navigation stack. A stack is a useful collection model whenever you need a strict “last in, first out” approach to managing a collection.
 
+//Here’s how to write a nongeneric version of a stack, in this case for a stack of Int values:
+
+struct IntStack {
+    var items = [Int]()
+    mutating func push(_ item: Int) {
+        items.append(item)
+    }
+    mutating func pop() -> Int {
+        return items.removeLast()
+    }
+}
+
+func showImage(with url: String) -> UIImage{
+    let imageUrl = URL(string: url)
+    let data = try? Data(contentsOf: imageUrl!) //make sure your image in this url does exist, otherwise unwrap in a if let check / try-catch
+    if let dataImage = data, let image = UIImage(data: dataImage) {
+        return image
+    }
+    
+    return UIImage()
+}
+
+showImage(with: "https://docs.swift.org/swift-book/_images/stackPushPop_2x.png")
+
+//This structure uses an Array property called items to store the values in the stack. Stack provides two methods, push and pop, to push and pop values on and off the stack. These methods are marked as mutating, because they need to modify (or mutate) the structure’s items array.
+//
+//The IntStack type shown above can only be used with Int values, however. It would be much more useful to define a generic Stack class, that can manage a stack of any type of value.
+//
+//Here’s a generic version of the same code:
+
+struct Stack<Element> {
+    var items = [Element]()
+    mutating func push(_ item: Element) {
+        items.append(item)
+    }
+    mutating func pop() -> Element {
+        return items.removeLast()
+    }
+}
+
+//
+//Note how the generic version of Stack is essentially the same as the nongeneric version, but with a type parameter called Element instead of an actual type of Int. This type parameter is written within a pair of angle brackets (<Element>) immediately after the structure’s name.
+
+//Element defines a placeholder name for a type to be provided later. This future type can be referred to as Element anywhere within the structure’s definition. In this case, Element is used as a placeholder in three places:
+//
+//To create a property called items, which is initialized with an empty array of values of type Element
+//To specify that the push(_:) method has a single parameter called item, which must be of type Element
+//To specify that the value returned by the pop() method will be a value of type Element
+//Because it’s a generic type, Stack can be used to create a stack of any valid type in Swift, in a similar manner to Array and Dictionary.
+//
+//You create a new Stack instance by writing the type to be stored in the stack within angle brackets. For example, to create a new stack of strings, you write Stack<String>():
+
+var stackOfStrings = Stack<String>()
+stackOfStrings.push("uno")
+stackOfStrings.push("dos")
+stackOfStrings.push("tres")
+stackOfStrings.push("cuatro")
+
+// Can stack suport other types ?
+//Proof:
+
+var stackOfInt = Stack<Int>()
+
+stackOfInt.push(1)
+stackOfInt.push(2)
+stackOfInt.push(3)
+stackOfInt.push(4)
+
+//Here’s how stackOfStrings looks after pushing these four values on to the stack:
+
+showImage(with: "https://docs.swift.org/swift-book/_images/stackPushedFourStrings_2x.png")
+
+//Popping a value from the stack removes and returns the top value, "cuatro":
+
+let fromTheTop = stackOfStrings.pop()
+
+//Here's how the stack looks after popping its top value:
+
+showImage(with: "https://docs.swift.org/swift-book/_images/stackPoppedOneString_2x.png")
+
+/*Extending a Generic Type*/
