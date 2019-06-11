2c6092e4fcfa45fed6a7e22d7ae347474294b043
josevictor1 <josevictorpatos@gmail.com>
2019-06-11 09:26:05 -0300

    add Handling Errors

--- TheSwiftProgrammingLanguage.playground/Pages/ErrorHandling.xcplaygroundpage/Contents.swift
+++ TheSwiftProgrammingLanguage.playground/Pages/ErrorHandling.xcplaygroundpage/Contents.swift
@@ -1,7 +1,36 @@
-//: [Previous](@previous)
+// Error Handling
 
-import Foundation
+//Error handling is the process of responding to and recovering from error conditions in your program. Swift provides first-class support for throwing, catching, propagating, and manipulating recoverable errors at runtime.
 
-var str = "Hello, playground"
+//Some operations aren’t guaranteed to always complete execution or produce a useful output. Optionals are used to represent the absence of a value, but when an operation fails, it’s often useful to understand what caused the failure, so that your code can respond accordingly.
 
-//: [Next](@next)
+//As an example, consider the task of reading and processing data from a file on disk. There are a number of ways this task can fail, including the file not existing at the specified path, the file not having read permissions, or the file not being encoded in a compatible format. Distinguishing among these different situations allows a program to resolve some errors and to communicate to the user any errors it can’t resolve.
+
+//NOTE
+
+//Error handling in Swift interoperates with error handling patterns that use the NSError class in Cocoa and Objective-C. For more information about this class, see Handling Cocoa Errors in Swift.
+
+//Representing and Throwing Errors
+
+
+//In Swift, errors are represented by values of types that conform to the Error protocol. This empty protocol indicates that a type can be used for error handling.
+
+//Swift enumerations are particularly well suited to modeling a group of related error conditions, with associated values allowing for additional information about the nature of an error to be communicated. For example, here’s how you might represent the error conditions of operating a vending machine inside a game:
+
+enum VendingMachineError: Error {
+    case invalidSelection
+    case insufficientFunds(coinsNeeded: Int)
+    case outOfStock
+}
+
+//Throwing an error lets you indicate that something unexpected happened and the normal flow of execution can’t continue. You use a throw statement to throw an error. For example, the following code throws an error to indicate that five additional coins are needed by the vending machine:
+
+throw VendingMachineError.insufficientFunds(coinsNeeded: 5)
+
+//Perfect!
+//Playground execution terminated: An error was thrown and was not caught:
+//▿ VendingMachineError
+//▿ insufficientFunds : 1 element
+//- coinsNeeded : 5
+
+//Handling Errors
