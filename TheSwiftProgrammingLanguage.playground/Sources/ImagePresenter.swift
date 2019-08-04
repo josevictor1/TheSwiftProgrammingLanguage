4a0c7bedcc0db2b117096315ef528d0cbd62ad18
josevictor1 <josevictorpatos@gmail.com>
2019-08-04 15:30:18 -0300

    Breaking Closure's Strong Reference Cycle

--- TheSwiftProgrammingLanguage.playground/Sources/ImagePresenter.swift
+++ TheSwiftProgrammingLanguage.playground/Sources/ImagePresenter.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-public class ImageDrawer {
+public class ImagePresenter {
     public static func showImage(with url: String) -> UIImage {
         guard let imageUrl = URL(string: url) else { return UIImage() }
         let data = try? Data(contentsOf: imageUrl) //make sure your image in this url does exist, otherwise unwrap in a if let check / try-catch
