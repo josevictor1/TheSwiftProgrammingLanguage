7132f028b23ea3653fc421e727fe6ec528e2b54e
josevictor1 <josevictorpatos@gmail.com>
2019-08-21 13:58:04 -0300

    Memory safety

--- TheSwiftProgrammingLanguage.playground/Pages/MemorySafety.xcplaygroundpage/Contents.swift
+++ TheSwiftProgrammingLanguage.playground/Pages/MemorySafety.xcplaygroundpage/Contents.swift
@@ -1,2 +1,7 @@
 // Memory Safety
 
+// Basically, the memory safety  is focused in the permission type to handle a resource, and this resoucre is memory of course...
+
+// The most part of the problems are caused by acess/permission conflict.
+
+//
